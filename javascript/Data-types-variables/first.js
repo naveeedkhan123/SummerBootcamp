e84d7f4d33f9b573b0a5a056658e5d476827adcc
Naveed khane <naveeedkhan1237@gmail.com>
2026-06-20 04:08:56 -0700

    learned about data variables,data types , oject,how to change value of key value in object

---
+++ javascript/Data-types-variables/first.js
@@ -0,0 +1,199 @@
+console.log("Hello hey everyone");
+alert("hello");
+console.log("easy game");
+console.log("i love web development");
+
+// variables in js
+
+name ="Joss Buttler";
+console.log(name);
+
+
+Name=("Naveed khan");
+console.log(Name);
+
+
+NAME = ("Raees Khan");
+console.log(NAME)
+
+
+age ="25";
+console.log(age);
+
+
+role ="Batter"
+console.log(role);
+
+
+x=null;
+console.log(x);
+
+
+y=("undefine");
+console.log(y);
+
+
+isFollow="false";
+console.log(isFollow);
+
+
+isPass="true";
+console.log(isPass);
+
+
+isGenious="yess";
+console.log(isGenious);
+
+
+isGiveUp="no and never";
+console.log(isGiveUp);
+
+
+// key words in js
+
+let number ="23";
+console.log(number);
+
+let a ="hello";
+console.log("a");
+
+var b ="javascript";
+console.log(b);
+
+{
+    let n="naveed khan";
+    console.log(n);
+}
+
+{
+    let n ="naveed khan";
+    console.log(n);
+}
+
+
+
+var c ="web development";
+console.log(c);
+
+
+
+
+const fatherName="Naimat khan";
+console.log(fatherName);
+
+const gender="male";
+console.log(gender);
+
+const PI="3.14";
+console.log(PI);
+
+
+
+
+
+
+// Data Types in JS
+
+// number
+
+let followers = 100000;
+
+let totalPrice= 99999 ;
+
+let z =222;
+
+// string
+
+{
+    let name ="joss buttler";
+    console.log(name);
+
+    let fullName ="Naveed khan";
+    console.log(fullName);
+}
+
+
+// Boolean
+
+isWorking="true"
+console.log(isWorking);
+
+isHot="yess";
+console.log(isHot);
+
+isHungry="no"
+console.log(isHungry);
+
+// undefined
+
+let h;
+console.log(h)
+
+let phoneNumber;
+console.log(phoneNumber);
+
+// null
+
+let p=null;
+let statuss=null;
+
+// BigInt
+
+let s=BigInt("123");
+
+let l=Symbol("hello!");
+
+
+
+// object
+
+const student =  {
+    fullName : "Naveed Khan",
+    age : 20 ,
+    cgpa : 3.20 ,
+    isPass : true,
+} ;
+
+console.log(student["cgpa"]);
+console.log(student["fullName"]);
+console.log(student["isPass"])
+
+// how to change age in object
+student["age"] = student["age"] +1;
+console.log (student["age"]);
+
+// how to change name in Object
+student["fullName"] = "Raees Khan";
+console.log (student["fullName"]);
+
+
+// Practice question
+
+const product = {
+fullName:"Pen",
+rating:4,
+IsDeal : true,
+price:270 ,
+offer:5,
+} ;
+
+console.log("product");
+
+
+// Practice question
+
+const profile = {
+    userName :"@Naveed khan",
+    posts:195,
+    followers:569000,
+    following:4,
+    isFollow:true,
+    isVerified:true,
+}
+
+console.log("profile");
+
+
+// how to check type of veriable in object
+
+console.log(typeof profile["userName"]);
