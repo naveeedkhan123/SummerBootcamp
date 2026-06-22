f9bcf21a4cc594187739d29dc95302c13a188c10
Naveed khane <naveeedkhan1237@gmail.com>
2026-06-22 07:20:58 -0700

    Learned and practice data types , data variables , loops, strings,operators,condition statement and arrays with examples

---
+++ javascript/loops-strings/practice.js
@@ -0,0 +1,368 @@
+//   Loops and JavaScript
+
+{
+    let name ="Naveed khan"
+    // console.log(name.length)
+}
+
+
+
+{
+    let city ="Abbottabad";
+    // console.log(city.toUpperCase());
+    // console.log(city.toLowerCase());
+}
+
+
+
+{
+    let word ="JavaScript";
+    // console.log(word[0])
+    //  console.log(word[4])
+}
+
+
+
+{
+
+    let str="hello";
+    let reverse ="";
+
+    for (let i = str.length -1; i>=0;i--) {
+        reverse += str[i];
+    }
+//     console.log(reverse);
+}
+
+
+
+
+{
+    for (let i =1; i<=10; i++){
+        // console.log(i);
+        }
+}
+// console.log("loop end here");
+
+
+
+
+
+{
+for (let i = 2;i<=13;i++){
+    // console.log(i);
+}
+// console.log("loop end here");
+}
+
+
+
+
+{
+    for (let i =3 ;i<=24; i+=3){
+        // console.log(i);
+    }
+    
+}
+
+
+
+{
+    for (let i = 1 ; i<=10 ; i++){
+    //     console.log(`7*${i} = ${7*i}`);
+    }
+}
+
+
+{
+
+    let name = "Pakistan";
+    for (let i = 0; i< name.length;i++){
+        // console.log(name[i]);
+    };
+}
+
+
+
+{
+    let name ="Naveed khan"
+    for (let i=0;i<name.length;i++){
+        // console.log(name[i]);
+    }
+}
+
+{
+    let str ="JavaScript";
+    let count = 0;
+    for (let i=0;i<str.length;i++){
+        if(
+            str[i]==="a"   ||
+            str[i]==="e"   ||
+            str[i]==="i"   ||
+            str[i]==="o"   ||
+            str[i]==="u"   
+        ) {
+            count++;
+        }
+    }
+// console.log("vowels =",count);
+}
+
+
+
+{
+    let str = "Developers";
+    let reverse ="";
+    for (let i=str.length-1;i>=0;i--) {
+        reverse += str[i];
+    }
+    // console.log(reverse);
+}
+
+
+
+{
+    let str = "Programming";
+    let count = 0;
+
+    for(i=0;i<str.length;i++){
+        // console.log(str[length]);
+    }
+}
+
+
+{
+    let str ="hello";
+    for (i=0;i<str.length;i++){
+        // console.log(str[i]);
+    }
+}
+
+{
+    for(let i=1; i<=50; i++){
+        if(i%2 !== 0){
+           // console.log(i)
+        }
+    }
+}
+
+{
+    let str = "Roman Reigns"
+    let count =0;
+    for(i=0;i<str.length;i++){
+        // console.log(str[length]);
+    }
+}
+
+
+
+{
+    let str ="Software Engineering";
+    let count=0;
+    for (let i =0;i<str.length;i++){
+        if(
+            str[i]==="a"   ||
+            str[i]==="e"   ||
+            str[i]==="i"   ||
+            str[i]==="o"   ||
+            str[i]==="u"   
+        ){
+            count++;
+        }
+    }
+    //console.log("vowels=",count);
+}
+
+
+
+{
+    let str="Raees khan"
+    let reverse="";
+    for(let i=str.length-1;i>=0;i--){
+reverse+=str[i];
+    }
+    //console.log(reverse);
+}
+
+
+
+{
+    let str ="Pakistan";
+    let count =0;
+    for (let i =0;i<=str.length;i++){
+        if(
+            str[i]==="a"
+        ){
+           // console.log(str[i])
+        }
+    }
+
+}
+
+{
+    let a = 2;
+    let b = 4;
+
+    b = b + a;
+//     console.log(b);
+    // b += a;
+    // console.log(b)
+
+}
+
+
+
+{
+    let a = 3;
+    let b = 4;
+    //console.log("a  =",a,"b =",b)
+    //console.log("a+b =",a+b);
+}
+
+{
+    let a = 7;
+    let b = 3;
+    //console.log("a  =",a,"b =",b)
+    //console.log("a*b =",a*b);
+}
+
+
+{
+    let a = 9;
+    let b = 4;
+    //console.log("a  =",a,"b =",b)
+   // console.log("a-b =",a-b);
+}
+
+{
+    let a = 21;
+    let b = 3;
+    //console.log("a  =",a,"b =",b)
+    //console.log("a/b =",a/b);
+}
+
+
+
+{
+    let name ="naveed khan";
+    let age =23;
+    let isStudentPass="Yes";
+
+    //console.log(name);
+    //console.log(age);
+    //console.log(isStudentPass);
+}
+
+{
+    let City ="Abbottabad";
+    let temperature=32;
+    let Raining ="Yess its Raining";
+    //console.log(City);
+    //console.log(temperature);
+    //console.log(Raining);
+}
+
+
+// {
+//     let age= 19;
+//     if (age<18){
+//         console.log("you can not vote ");
+//     }else
+//     console.log("you can vote");
+// }
+
+
+// {
+//     let number =6;
+//     if(number%2 === 0){
+//         console.log("Even Number");
+//     }else 
+//         console.log("Odd Number");
+// }
+
+
+
+
+//     let marks =40;
+//     if (marks>50){
+//         console.log("You Are Pass");
+//     } else
+//         console.log("You Are Failed");
+// }
+
+{
+    for (let i=1 ; i<=5;i++);
+       // console.log(i)
+}
+
+
+{
+    for (let i = 2; i <= 10; i += 2) {
+   // console.log(i);
+}
+}
+
+
+{
+    let name ="Pakistan";
+    //console.log(name.length);
+    //console.log(name[0]);
+     //console.log(name[7]);
+}
+
+{
+    let str ="hello";
+    for(let i=0;i<str.length;i++) {
+        //console.log(str[i]);
+    }
+}
+
+
+
+{
+let name ="Joss Buttler";
+let reverse="";
+
+for(let i=name.length-1;i>=0;i--){
+    reverse +=  name[i];
+}
+//console.log(reverse);
+
+}
+
+
+{
+    let arr = [10,20,30,40,50]
+    //console.log(arr[0])
+      // console.log(arr[3])
+}
+
+
+{
+     let arr = [10,20,30,40,50,60,70,80];
+     for (i=0;i<arr.length;i++){
+       // console.log(arr[i]);
+     }
+}
+
+
+{
+     let arr = ["Naveed Khan","Apple","Phone","Laptop"];
+     for (i=0;i<arr.length;i++){
+       // console.log(arr[i]);
+     }
+}
+
+
+
+{
+     let arr = [10,20,30,40,50,60,70,80];
+     let sum =0;
+     for (i=0;i<arr.length;i++){
+        sum += arr[i]
+     }
+    // console.log(sum);
+}
+
+
+
